ab464463d0aaf9653d291884d3a2bf4f04323cbb
Robin Renggli <robin.k.renggli@gmail.com>
2022-01-15 12:08:10 +0100

    add movement timer

--- GodotProjectFolder/BasicMovement.gd
+++ GodotProjectFolder/BasicMovement.gd
@@ -6,3 +6,13 @@ var velocity = Vector2(1,0)
 func move(creature, speed):
 	creature.move_and_collide(velocity * speed)
 
+func change_direction():
+	velocity.x = rand_range(-1,1)
+	velocity.y = rand_range(-1,1)
+	velocity = velocity.normalized()
+
+
+func _on_Timer_timeout():
+	velocity.x = rand_range(-1,1)
+	velocity.y = rand_range(-1,1)
+	velocity = velocity.normalized()
