16d5ac9ace4a7cab758884a566c709525e13df42
Robin Renggli <robin.k.renggli@gmail.com>
2022-01-15 16:50:05 +0100

    fix sprite rotation

--- GodotProjectFolder/BasicMovement.gd
+++ GodotProjectFolder/BasicMovement.gd
@@ -1,18 +1,19 @@
 extends Node
 
 onready var creature = self.get_parent()
+onready var creatureSprite = creature.get_node("Sprite")
 onready var directionIndicator = Vector2(Constants.window_width/2, Constants.window_height/2)
+
 
 var velocity = Vector2(1,0)
 var directionIndicatorVelocity = Vector2(1,0)
 
-func move(speed, CreatureSprite):
+func move(speed):
 	directionIndicator = directionIndicator + directionIndicatorVelocity * speed * 2
-	if out_of_bounds():
-		directionIndicator = Vector2(rand_range(0, Constants.window_width), rand_range(0, Constants.window_height))
-		
+	handle_out_of_bounds()
+	
 	velocity = (directionIndicator - creature.global_position).normalized()
-	CreatureSprite.rotation = velocity.angle()
+	rotate_sprite()
 	
 	var collision = creature.move_and_collide(velocity * speed)
 	
@@ -24,9 +25,16 @@ func move(speed, CreatureSprite):
 func _on_Timer_timeout():
 	directionIndicatorVelocity = Vector2(rand_range(-1,1), rand_range(-1,1)).normalized()
 	
-func out_of_bounds():
-	if  (directionIndicator.x > Constants.window_width || directionIndicator.x < 0):
-		return true
-	if (directionIndicator.y > Constants.window_height || directionIndicator.y < 0):
-		return true
-	return false
+func handle_out_of_bounds():
+	if  directionIndicator.x > Constants.window_width || directionIndicator.x < 0:
+		directionIndicatorVelocity.x *= -1
+	if directionIndicator.y > Constants.window_height || directionIndicator.y < 0:
+		directionIndicatorVelocity.y *= -1
+	
+func rotate_sprite():
+	if velocity.x > 0:
+		creatureSprite.set_flip_h(false)
+		creatureSprite.rotation = velocity.angle()
+	else:
+		creatureSprite.set_flip_h(true)
+		creatureSprite.rotation = (velocity * -1).angle()
--- GodotProjectFolder/Creatures/Creature.gd
+++ GodotProjectFolder/Creatures/Creature.gd
@@ -2,13 +2,12 @@ extends KinematicBody2D
 
 onready var Stats = $Stats
 onready var Movement = $Movement
-onready var CreatureSprite = $Sprite
 
 func _ready():
 	pass # Replace with function body.
 	
 func _physics_process(delta):
-	Movement.move(Stats.speed * delta * Constants.delta_factor, CreatureSprite)
+	Movement.move(Stats.speed * delta * Constants.delta_factor)
 
 func _on_Body_area_entered(area):
 	PlayerStats.change_currency(1)
